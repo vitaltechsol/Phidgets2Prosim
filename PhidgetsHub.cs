11cd63132817d206a4e568be4f491e48c0ea63d4
vitaltechsol <info@vts.us>
2023-11-29 21:08:05 -0800

    feat: Add hub and port classes

---
+++ PhidgetsHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phidgets2Prosim
+{
+    internal class PhidgetsHub
+    {
+        public PhidgetsPort[] port;
+
+        public PhidgetsHub() {
+
+            port = new PhidgetsPort[6];
+            for (int i = 0; i < 6; i++)
+            {
+                port[i] = new PhidgetsPort();
+            }
+        }
+    }
+}
---
+++ PhidgetsPort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phidgets2Prosim
+{
+    internal class PhidgetsPort
+    {
+        public PhidgetsInput[] input;
+        public PhidgestOutput[] output;
+
+        public PhidgetsPort() {
+          input = new PhidgetsInput[16];
+          output = new PhidgestOutput[16];
+        }
+    }
+}
