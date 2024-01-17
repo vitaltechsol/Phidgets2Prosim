52715aae1fbcb2fb35dce51d503802deb6aee9da
vitaltechsol <info@vts.us>
2024-01-16 20:11:15 -0800

    Update definition

--- Form1.cs
+++ Form1.cs
@@ -146,9 +146,11 @@ namespace Phidgets2Prosim
                     var ph = new PhidgetsHub[3];
                     var mip1 = ph[0];
                     var mip2 = ph[1];
+                    var hub_ped = ph[2];
 
                     mip1 = new PhidgetsHub();
                     mip2 = new PhidgetsHub();
+                    hub_ped = new PhidgetsHub();
 
                     mip1.port[1].output[3] = new PhidgestOutput(hubMip, 1, 3, "system.indicators.I_MIP_ASA_ATR_1", connection);
                     mip1.port[1].output[4] = new PhidgestOutput(hubMip, 1, 4, "system.indicators.I_MIP_ASA_FMC_1", connection);
@@ -163,8 +165,8 @@ namespace Phidgets2Prosim
                     mip1.port[1].output[12] = new PhidgestOutput(hubMip, 1, 12, "system.indicators.I_MIP_GEAR_LEFT_TRANSIT", connection);
                     mip1.port[1].output[13] = new PhidgestOutput(hubMip, 1, 13, "system.indicators.I_MIP_GEAR_LEFT_DOWN", connection);
                     mip1.port[1].output[14] = new PhidgestOutput(hubMip, 1, 14, "system.gates.B_GEAR_HANDLE_RELEASE", connection);
-                    mip1.port[1].output[15] = new PhidgestOutput(hubMip, 1, 15, "system.gates.B_STANDBY_POWER", connection);
-                    mip1.port[1].output[15].Inverse = true;
+                    //    mip1.port[1].output[15] = new PhidgestOutput(hubMip, 1, 15, "system.gates.B_STANDBY_POWER", connection);
+                    //    mip1.port[1].output[15].Inverse = true;
 
                     mip2.port[0].output[0] = new PhidgestOutput(hubMip2, 0, 0, "system.indicators.I_MIP_LE_FLAP_TRANSIT", connection);
                     mip2.port[0].output[1] = new PhidgestOutput(hubMip2, 0, 1, "system.indicators.I_MIP_LE_FLAP_EXT", connection);
@@ -190,12 +192,12 @@ namespace Phidgets2Prosim
                     PhidgestOutput digitalOutput_3_2 = new PhidgestOutput(hubPedestal, 3, 2, "system.gates.B_REVERSER_1_SYNC_LOCK", connection, true);
                     digitalOutput_3_2.AddDelay(1500);
                     PhidgestOutput digitalOutput_3_3 = new PhidgestOutput(hubPedestal, 3, 3, "system.gates.B_AC_POWER", connection, true);
-                   digitalOutput_3_8 = new PhidgestOutput(hubPedestal, 3, 8,
-                        "system.gates.B_SPEED_BRAKE_DEPLOY", connection, true,
-                        "system.gates.B_SPEED_BRAKE_RESTOW");
+                    digitalOutput_3_8 = new PhidgestOutput(hubPedestal, 3, 8,
+                         "system.gates.B_SPEED_BRAKE_DEPLOY", connection, true,
+                         "system.gates.B_SPEED_BRAKE_RESTOW");
 
                     // Auto stow speed brake after it has open for that long
-                        digitalOutput_3_8.TurnOffAfterMs = 60000;
+                    digitalOutput_3_8.TurnOffAfterMs = 60000;
 
                     PhidgestOutput digitalOutput_3_7 = new PhidgestOutput(hubPedestal, 3, 7, "system.indicators.I_MIP_PARKING_BRAKE", connection);
 
@@ -220,20 +222,27 @@ namespace Phidgets2Prosim
                     PhidgestOutput digitalOutput_5_02 = new PhidgestOutput(hubPedestal, 5, 1, "system.indicators.I_ASP_VHF_2_SEND", connection);
 
 
+
+                    //hub_ped.port[3].input[0] = new PhidgetsInput(2, 0, "system.switches.S_FIRE_FAULT_TEST", 1, connection);
+                    // hub_ped.port[3].input[0] = new PhidgetsInput(2, 1, "system.switches.S_FIRE_FAULT_TEST", 2, connection);
+
                     digitalInput_2_00 = new PhidgetsInput(2, 0, "system.switches.S_FIRE_FAULT_TEST", 1, connection);
                     digitalInput_2_01 = new PhidgetsInput(2, 1, "system.switches.S_FIRE_FAULT_TEST", 2, connection);
                     digitalInput_2_02 = new PhidgetsInput(2, 2, "system.switches.S_FIRE_HANDLE1", 1, connection);
                     digitalInput_2_03 = new PhidgetsInput(2, 3, "system.switches.S_FIRE_PULL1", 1, connection);
                     digitalInput_2_04 = new PhidgetsInput(2, 4, "system.switches.S_FIRE_HANDLE1", 2, connection);
-
                     digitalInput_2_08 = new PhidgetsInput(2, 8, "system.switches.S_FIRE_HANDLE_APU", 1, connection);
                     digitalInput_2_09 = new PhidgetsInput(2, 9, "system.switches.S_FIRE_PULL_APU", 1, connection);
                     digitalInput_2_10 = new PhidgetsInput(2, 10, "system.switches.S_FIRE_HANDLE_APU", 2, connection);
-
-
                     digitalInput_2_11 = new PhidgetsInput(2, 11, "system.switches.S_FIRE_HANDLE2", 1, connection);
                     digitalInput_2_12 = new PhidgetsInput(2, 12, "system.switches.S_FIRE_PULL2", 1, connection);
                     digitalInput_2_13 = new PhidgetsInput(2, 13, "system.switches.S_FIRE_HANDLE2", 2, connection);
+
+                    //hub_ped.port[1].input[0] = new PhidgetsInput(1, 0, "system.switches.S_THROTTLE_FUEL_CUTOFF1", 1, connection);
+                    //hub_ped.port[1].input[1] = new PhidgetsInput(1, 1, "system.switches.S_THROTTLE_FUEL_CUTOFF2", 1, connection);
+                    //hub_ped.port[1].input[2] = new PhidgetsInput(1, 2, "system.switches.S_THROTTLE_AT_DISENGAGE", 1, connection);
+                    //hub_ped.port[1].input[3] = new PhidgetsInput(1, 3, "system.switches.S_THROTTLE_AT_DISENGAGE_2", 1, connection);
+                    //hub_ped.port[1].input[4] = new PhidgetsInput(1, 4, "system.switches.S_THROTTLE_TOGA", 1, connection);
 
                     digitalInput1_0 = new PhidgetsInput(1, 0, "system.switches.S_THROTTLE_FUEL_CUTOFF1", 1, connection);
                     digitalInput1_1 = new PhidgetsInput(1, 1, "system.switches.S_THROTTLE_FUEL_CUTOFF2", 1, connection);
@@ -241,10 +250,10 @@ namespace Phidgets2Prosim
                     digitalInput1_3 = new PhidgetsInput(1, 3, "system.switches.S_THROTTLE_AT_DISENGAGE_2", 1, connection);
                     digitalInput1_4 = new PhidgetsInput(1, 4, "system.switches.S_THROTTLE_TOGA", 1, connection);
 
-                    digitalInput1_10 = new PhidgetsInput(1, 10, "system.switches.S_RUDDER_TRIM", 1, connection);
-                    digitalInput1_11 = new PhidgetsInput(1, 11, "system.switches.S_RUDDER_TRIM", 2, connection);
                     digitalInput1_08 = new PhidgetsInput(1, 08, "system.switches.S_AILERON_TRIM", 1, connection);
                     digitalInput1_09 = new PhidgetsInput(1, 09, "system.switches.S_AILERON_TRIM", 2, connection);
+                    digitalInput1_10 = new PhidgetsInput(1, 10, "system.switches.S_RUDDER_TRIM", 1, connection);
+                    digitalInput1_11 = new PhidgetsInput(1, 11, "system.switches.S_RUDDER_TRIM", 2, connection);
 
                     digitalInput1_12 = new PhidgetsInput(1, 12, "system.switches.S_ASP_VHF_1_SEND", 1, connection);
                     digitalInput1_13 = new PhidgetsInput(1, 13, "system.switches.S_ASP_VHF_2_SEND", 1, connection);
@@ -252,12 +261,12 @@ namespace Phidgets2Prosim
                     PhidgetsVoltageOutput pvo = new PhidgetsVoltageOutput(2, "system.gauge.G_MIP_BRAKE_PRESSURE", connection);
                     trimWheel = new Custom_TrimWheel(0, connection, 1, 0.8, 0.5, 0.5, 0.7, 0.3);
 
-                    bldcm_00 = new PhidgetsBLDCMotor(1, connection, false, 0,
+                    bldcm_00 = new PhidgetsBLDCMotor(0, connection, false, 0,
                         "system.gates.B_THROTTLE_SERVO_POWER_RIGHT",
                         "system.analog.A_THROTTLE_RIGHT",
                         "system.gauge.G_THROTTLE_RIGHT");
 
-                    bldcm_01 = new PhidgetsBLDCMotor(0, connection, true, 5,
+                    bldcm_01 = new PhidgetsBLDCMotor(1, connection, true, 5,
                      "system.gates.B_THROTTLE_SERVO_POWER_LEFT",
                      "system.analog.A_THROTTLE_LEFT",
                      "system.gauge.G_THROTTLE_LEFT");
@@ -298,17 +307,17 @@ namespace Phidgets2Prosim
             Debug.WriteLine("**** restart phidgets: ");
 
 
-            digitalInput1_0.Close();
-            digitalInput1_1.Close();
-            digitalInput1_2.Close();
-            digitalInput1_3.Close();
-            digitalInput1_4.Close();
+            //digitalInput1_0.Close();
+            //digitalInput1_1.Close();
+            //digitalInput1_2.Close();
+            //digitalInput1_3.Close();
+            //digitalInput1_4.Close();
 
-            digitalInput1_0.Open();
-            digitalInput1_1.Open(); 
-            digitalInput1_2.Open();
-            digitalInput1_3.Open(); 
-            digitalInput1_4.Open();
+            //digitalInput1_0.Open();
+            //digitalInput1_1.Open(); 
+            //digitalInput1_2.Open();
+            //digitalInput1_3.Open(); 
+            //digitalInput1_4.Open();
         }
 
 
@@ -323,9 +332,9 @@ namespace Phidgets2Prosim
                     Debug.WriteLine("Sim paused Changed " + dataRef.value + " " + dataRef.name);
 
                     // Pause motors
-                   // trimWheel.pause(simIsPaused);
-                    bldcm_00.pause(simIsPaused);
-                    bldcm_01.pause(simIsPaused);
+                  //  trimWheel.pause(simIsPaused);
+                    // bldcm_00.pause(simIsPaused);
+                    // bldcm_01.pause(simIsPaused);
 
                     Invoke(new MethodInvoker(updateStatusLabel));
                 }
