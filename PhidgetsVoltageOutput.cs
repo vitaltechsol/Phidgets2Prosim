ff512bc9e59d5988b19229002d22e4f1cdc06b64
vitaltechsol <info@vts.us>
2024-07-15 20:39:21 -0700

    Add soothing to voltage output for indicators

--- PhidgetsVoltageOutput.cs
+++ PhidgetsVoltageOutput.cs
@@ -5,6 +5,9 @@ using System;
 using System.Runtime.Remoting.Channels;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Timers;
+using YamlDotNet.Core.Tokens;
+using System.Xml.Linq;
 
 namespace Phidgets2Prosim
 {
@@ -15,11 +18,22 @@ namespace Phidgets2Prosim
         public double ScaleFactor { get; set; }
         public double Offset { get; set; } = 0;
 
+        public int Interval { get; set; } = 10;  // Timer interval in milliseconds
+
+        public double SmoothFactor { get; set; } = 0.1;  // Smooth factor for gradual movement
+
         double lastVoltage = 0;
+
+        private double currentPosition = 0;
+        private double targetPosition = 0;
+        private Timer timer;
 
         public PhidgetsVoltageOutput(int deviceSerialN, int hubPort, string prosimDatmRef, ProSimConnect connection)
         {
-
+            timer = new Timer(Interval);
+            timer.Elapsed += OnTimerElapsed;
+            timer.AutoReset = true;
+            
             try
             {
                 voltageOutput.DeviceSerialNumber = deviceSerialN; 
@@ -29,7 +43,7 @@ namespace Phidgets2Prosim
                 Open();
 
                 // Set ProSim dataref
-                DataRef dataRef = new DataRef(prosimDatmRef, 100, connection);
+                DataRef dataRef = new DataRef(prosimDatmRef, 5, connection);
                 dataRef.onDataChange += DataRef_onDataChange;
             }
             catch (Exception ex)
@@ -59,35 +73,77 @@ namespace Phidgets2Prosim
             }
         }
 
-        private void DataRef_onDataChange(DataRef dataRef)
+        public void  GotoValue(double value, string name )
         {
-            var value = Convert.ToInt64(dataRef.value);
-            var convertedValue = value > 0 ? (value / ScaleFactor) : 0;
+            var convertedValue = value > 0 ? Math.Round((value / ScaleFactor),2) : 0;
             convertedValue = convertedValue + Offset;
             if (convertedValue > 10 || convertedValue < -10)
             {
-                Debug.WriteLine($"Voltage invalid: {dataRef.name} | value: {value} | Converted {convertedValue} | ScaleFactor: {ScaleFactor} | Offset {Offset}");
+                Debug.WriteLine($"Voltage invalid: {name} | value: {value} | Converted {convertedValue} | ScaleFactor: {ScaleFactor} | Offset {Offset}");
                 convertedValue = 0;
             }
-            if (lastVoltage != value)
-            {
 
-                try
+            if (targetPosition != convertedValue)
+            {
+                Debug.WriteLine($"Voltage Changed: {name} {value} new target {convertedValue}");
+                targetPosition = convertedValue;
+                if (!timer.Enabled)
                 {
-                   Debug.WriteLine($"Voltage Changed: {dataRef.name} {value}");
-                   Debug.WriteLine(value);
-                   Debug.WriteLine(convertedValue);
-                   voltageOutput.Voltage = Convert.ToDouble(convertedValue);
-                
+                    timer.Start();
                 }
-                catch (Exception ex)
-                {
-                    SendErrorLog("ERROR: " + value + " to " + convertedValue);
-                    SendErrorLog(ex.ToString());
-                }
-                lastVoltage = value;
+                    
             }
         }
 
+        private void DataRef_onDataChange(DataRef dataRef)
+        {
+           var value = Math.Round(Convert.ToDouble(dataRef.value), 3);
+           //  Debug.WriteLine($"ataRef.value: {dataRef.value}");
+           GotoValue(value, dataRef.name );
+        }
+
+        private void UpdateNeedlePosition(double convertedValue)
+        {
+            try
+            {
+                Debug.WriteLine($"updating position to {convertedValue}");
+                voltageOutput.Voltage = Convert.ToDouble(convertedValue);
+
+            }
+            catch (Exception ex)
+            {
+                SendErrorLog("ERROR: " + convertedValue + " to " + targetPosition);
+                SendErrorLog(ex.ToString());
+            }
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // Move the needle towards the target position smoothly
+            if (targetPosition > currentPosition)
+            {
+                currentPosition += SmoothFactor;
+                // Stop the timer if the needle has reached the target position
+                if ((currentPosition >= targetPosition))
+                {
+                    currentPosition = targetPosition;
+                    Debug.WriteLine($"Target position, Stopping: {currentPosition} to {targetPosition}");
+                    timer.Stop();
+                }
+            } else
+            {
+                currentPosition -= SmoothFactor;
+                if ((currentPosition <= targetPosition))
+                {
+                    currentPosition = targetPosition;
+                    Debug.WriteLine($"Target position, Stopping: {currentPosition} to {targetPosition}");
+                    timer.Stop();
+                }
+            }
+
+            Debug.WriteLine($"Timer position set at {currentPosition} to {targetPosition}");
+            // Update the needle position here
+            UpdateNeedlePosition(currentPosition);
+        }
     }
 }
